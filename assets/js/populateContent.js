92d48405a9acca4e278c6cd5694b019b60f4e6b9
ron erlih <ronerlih@gmail.com>
2020-11-16 12:07:22 -0500

    page 2

---
+++ assets/js/populateContent.js
@@ -0,0 +1,63 @@
+const projectContainer = document.querySelector("#projects");
+const projectPage = document.querySelector("#project-content");
+const projectId = window.location.pathname.indexOf('project') > -1 ? parseInt(window.location.pathname.split(".")[window.location.pathname.split(".").length - 2].slice(-1)) : undefined;
+
+console.log(data)
+
+// populate home page
+projectContainer ? projectContainer.innerHTML = data.projects.map(
+   project => 
+   `
+   <a href=${ project.url }>
+				<table>
+					<tr>
+						<td class="featured-img">
+							<img src=${ project.thumb } title="Large Scale Drawing" />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span>${ project.name }</span>
+						</td>
+					</tr>
+				</table>
+			</a>
+
+   `).join('') : 0;
+
+   console.log(data.projects[projectId-1].name)
+// populate project page
+if (!isNaN(projectId))
+projectPage ? projectPage.innerHTML = 
+   // title
+   `<h1>${data.projects[projectId-1].name}</h1>` +
+
+   // main media
+   `<img class="mainImg" src=${data.projects[projectId-1].mainImg} />
+   <br />
+   ` +
+
+   // components
+   data.projects
+      .filter( project => project.id == projectId)[0]
+      .componenets
+      .map(componenet => 
+         componenet.p 
+            ? 
+            `<p>
+               ${ componenet.p.imgUrl 
+                  ? `<img src=${ componenet.p.imgUrl } /><br />`
+                  : "" }
+               <span class="image-title">
+                  ${ componenet.p.text }
+               </span>
+            </p>`
+            : 
+            `<video controls poster=${ componenet.video.poster } src=${ componenet.video.src }>
+               <span class="image-title">
+               ${ componenet.video.text }
+            </span>`
+      )
+      .join('')
+      : 0
+
--- data.js
+++ data.js
@@ -1,24 +1,108 @@
-const homepage = {
-   projects: [
-      {  url: "project_1.html",
-         thumb: "assets/project-1/Thumb.jpg",
-         name: "Large Scale Drawing"
+const data = {
+	projects: [
+		{
+			id: 1,
+			url: "pages/project_1.html",
+			thumb: "assets/project-1/Thumb.jpg",
+			name: "Large Scale Drawing",
+			mainImg: "../assets/project-1/1.jpg",
+			"image-title":
+				"Untitled, Large Scale Drawing, 2011, 250cm x 150cm, 8.2ft x 5ft<br />(perforated drawing)",
+			mainP: `I began developing my own drawing style in my A level year taking art. In my foundation
+         year at Camberwell College of Art - I completed my first large scale drawing. In this
+         drawing I experimented with perforating paper. I was very much drawn to the texture of
+         what the back of the paper felt like and the abstract pattern that was created.`,
+			componenets: [
+				{
+					p: {
+						text: `I began developing my own drawing style in my A level year taking art. In my foundation
+               year at Camberwell College of Art - I completed my first large scale drawing. In this
+               drawing I experimented with perforating paper. I was very much drawn to the texture of
+               what the back of the paper felt like and the abstract pattern that was created.`,
+					},
+				},
+				{
+					p: {
+						imgUrl: "../assets/project-1/2.jpg",
+						text: `Untitled, Pencil On Fabriano Paper 2011, 2 meters 80cm x 1 meter 57cm, 9.1ft x 5.1ft
+					<br />
+					(large scale pencil drawing with grey pencil)`,
+					},
+				},
+				{
+					p: {
+						imgUrl: "../assets/project-1/3.jpg",
+						text: `Untitled, Red Ink On Fabriano Paper 2010, 2 meters 50cm X 1 meters 50cm, 8.2ft x
+               4.9ft<br />
+               (Red ink on Fabriano paper)`,
+					},
+				},
+				{
+					p: {
+						imgUrl: "../assets/project-1/4.jpg",
+						text: `Untitled, Map Of Mount Etna, Black Ink On Printed Map 2012, 81cm X 81cm, 2.6ft x
+               2.6ft<br />
+               (Mount Etna drawing)`,
+					},
+				},
+				{
+					p: {
+						imgUrl: "../assets/project-1/5.jpg",
+						text: `Untitled, Biro on Fabriano Paper, 2011-2012, 1 meter 50 cm x 1 meter 50 cm, 4.9ft
+               x 4.9ft<br />
+               (Drawing below Mount Etna)`,
+					},
+				},
+			],
+		},
+		{
+			id: 2,
+			url: "pages/project_2.html",
+			thumb: "assets/project-2/Thumb.jpg",
+			name: "Goldsmiths Thesis show",
+         mainImg: "../assets/project-2/1.jpg",
+			"image-title":
+				"Untitled, Large Scale Drawing, 2011, 250cm x 150cm, 8.2ft x 5ft<br />(perforated drawing)",
+			mainP: `Untitled, Black Ink On White Paper 2013, 500cm x 100cm, 16.4ft x 3.2ft.`,
+			componenets: [
+				{
+					p: {
+						text: `My degree show at Goldsmiths was a exploration into my interest in light and life whilst also exploring life after death and the things that are revealed about someone after they die and the legacy someone leaves.`,
+					},
+				},
+				{
+					p: {
+						imgUrl: "../assets/project-2/2.jpg",
+						text: `Untitled, White Ink on Black Paper 2013, 500cm x 100cm, 16.4ft x 3.2ft`,
+					},
+				},
+				{
+					p: {
+						text: `Using the text piece as a starting point, laid to rest in darkness the light of her life, alongside my own curiosity into life and death, I began to think about how I could create a piece encompassing these interests.`,
+					},
+				},
+				{
+					p: {
+						text: `The text piece, I found in a newspaper. The newspaper was dedicated to Princess Diana after she died. Alongside the projected text, there were two large scale drawings of my interpretation of light. One was black ink on white paper, the other being white ink on black paper.
+                  Alongside the text pieces and drawings, there was a performance I created involving tissue paper and a overhead projector.`,
+					},
+				},
+				{
+					video: {
+                  poster: "assets/project-2/4.jpg",
+                  src: "assets/project-2/3.mov",
+                  text: ``,
+					},
+				},
+         ],
       },
-      {  url: "project_2.html",
-         thumb: "assets/project-2/Thumb.jpg",
-         name: "Goldsmith Thesis"
-      },
-      {  url: "project_3.html",
-         thumb: "assets/project-3/Thumb.jpg",
-         name: "Calasetta Calling"
-      },
-      {  url: "project_4.html",
-         thumb: "assets/project-4/Thumb.jpg",
-         name: "Brain"
-      },
-      {  url: "project_5.html",
-         thumb: "assets/project-5/Thumb.jpg",
-         name: "SVA"
-      }
-   ]
-}
+		{
+			id: 3,
+			url: "pages/project_3.html",
+			thumb: "assets/project-3/Thumb.jpg",
+			name: "Calasetta Calling",
+		},
+		{ id: 4, url: "pages/project_4.html", thumb: "assets/project-4/Thumb.jpg", name: "Brain" },
+		{ id: 5, url: "pages/project_5.html", thumb: "assets/project-5/Thumb.jpg", name: "SVA" },
+	],
+};

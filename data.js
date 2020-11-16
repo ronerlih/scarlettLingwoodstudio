567b861616e144b4894c18dee38c6bdbea8c7647
scarlettl123 <74564365+scarlettl123@users.noreply.github.com>
2020-11-16 15:15:42 -0500

    update data.js edit project 6

--- data.js
+++ data.js
@@ -1,217 +1,260 @@
 const data = {
-	projects: [
-		{
-			id: 1,
-			url: "pages/project_1.html",
-			thumb: "assets/project-1/Thumb.jpg",
-			name: "Large Scale Drawing",
-			mainImg: "../assets/project-1/1.jpg",
-			"image-title":
-				"Untitled, Large Scale Drawing, 2011, 250cm x 150cm, 8.2ft x 5ft<br />(perforated drawing)",
-			mainP: `I began developing my own drawing style in my A level year taking art. In my foundation
+    projects: [
+        {
+            id: 1,
+            url: "pages/project_1.html",
+            thumb: "assets/project-1/Thumb.jpg",
+            name: "Large Scale Drawing",
+            mainImg: "../assets/project-1/1.jpg",
+            "image-title":
+                "Untitled, Large Scale Drawing, 2011, 250cm x 150cm, 8.2ft x 5ft<br />(perforated drawing)",
+            mainP: `I began developing my own drawing style in my A level year taking art. In my foundation
          year at Camberwell College of Art - I completed my first large scale drawing. In this
          drawing I experimented with perforating paper. I was very much drawn to the texture of
          what the back of the paper felt like and the abstract pattern that was created.`,
-			componenets: [
-				{
-					p: {
-						text: `I began developing my own drawing style in my A level year taking art. In my foundation
+            componenets: [
+                {
+                    p: {
+                        text: `I began developing my own drawing style in my A level year taking art. In my foundation
                year at Camberwell College of Art - I completed my first large scale drawing. In this
                drawing I experimented with perforating paper. I was very much drawn to the texture of
                what the back of the paper felt like and the abstract pattern that was created.`,
-					},
-				},
-				{
-					p: {
-						imgUrl: "../assets/project-1/2.jpg",
-						text: `Untitled, Pencil On Fabriano Paper 2011, 2 meters 80cm x 1 meter 57cm, 9.1ft x 5.1ft
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-1/2.jpg",
+                        text: `Untitled, Pencil On Fabriano Paper 2011, 2 meters 80cm x 1 meter 57cm, 9.1ft x 5.1ft
 					<br />
 					(large scale pencil drawing with grey pencil)`,
-					},
-				},
-				{
-					p: {
-						imgUrl: "../assets/project-1/3.jpg",
-						text: `Untitled, Red Ink On Fabriano Paper 2010, 2 meters 50cm X 1 meters 50cm, 8.2ft x
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-1/3.jpg",
+                        text: `Untitled, Red Ink On Fabriano Paper 2010, 2 meters 50cm X 1 meters 50cm, 8.2ft x
                4.9ft<br />
                (Red ink on Fabriano paper)`,
-					},
-				},
-				{
-					p: {
-						imgUrl: "../assets/project-1/4.jpg",
-						text: `Untitled, Map Of Mount Etna, Black Ink On Printed Map 2012, 81cm X 81cm, 2.6ft x
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-1/4.jpg",
+                        text: `Untitled, Map Of Mount Etna, Black Ink On Printed Map 2012, 81cm X 81cm, 2.6ft x
                2.6ft<br />
                (Mount Etna drawing)`,
-					},
-				},
-				{
-					p: {
-						imgUrl: "../assets/project-1/5.jpg",
-						text: `Untitled, Biro on Fabriano Paper, 2011-2012, 1 meter 50 cm x 1 meter 50 cm, 4.9ft
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-1/5.jpg",
+                        text: `Untitled, Biro on Fabriano Paper, 2011-2012, 1 meter 50 cm x 1 meter 50 cm, 4.9ft
                x 4.9ft<br />
                (Drawing below Mount Etna)`,
-					},
-				},
-			],
-		},
-		{
-			id: 2,
-			url: "pages/project_2.html",
-			thumb: "../assets/project-2/Thumb.jpg",
-			name: "Goldsmiths Thesis show",
-         mainImg: "../assets/project-2/1.jpg",
-			"image-title":
-				"Untitled, Large Scale Drawing, 2011, 250cm x 150cm, 8.2ft x 5ft<br />(perforated drawing)",
-			mainP: `Untitled, Black Ink On White Paper 2013, 500cm x 100cm, 16.4ft x 3.2ft.`,
-			componenets: [
-				{
-					p: {
-						text: `My degree show at Goldsmiths was a exploration into my interest in light and life whilst also exploring life after death and the things that are revealed about someone after they die and the legacy someone leaves.`,
-					},
-				},
-				{
-					p: {
-						imgUrl: "../assets/project-2/2.jpg",
-						text: `Untitled, White Ink on Black Paper 2013, 500cm x 100cm, 16.4ft x 3.2ft`,
-					},
-				},
-				{
-					p: {
-						text: `Using the text piece as a starting point, laid to rest in darkness the light of her life, alongside my own curiosity into life and death, I began to think about how I could create a piece encompassing these interests.`,
-					},
-				},
-				{
-					p: {
-						text: `The text piece, I found in a newspaper. The newspaper was dedicated to Princess Diana after she died. Alongside the projected text, there were two large scale drawings of my interpretation of light. One was black ink on white paper, the other being white ink on black paper.
+                    },
+                },
+            ],
+        },
+        {
+            id: 2,
+            url: "pages/project_2.html",
+            thumb: "../assets/project-2/Thumb.jpg",
+            name: "Goldsmiths Thesis show",
+            mainImg: "../assets/project-2/1.jpg",
+            "image-title":
+                "Untitled, Large Scale Drawing, 2011, 250cm x 150cm, 8.2ft x 5ft<br />(perforated drawing)",
+            mainP: `Untitled, Black Ink On White Paper 2013, 500cm x 100cm, 16.4ft x 3.2ft.`,
+            componenets: [
+                {
+                    p: {
+                        text: `My degree show at Goldsmiths was a exploration into my interest in light and life whilst also exploring life after death and the things that are revealed about someone after they die and the legacy someone leaves.`,
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-2/2.jpg",
+                        text: `Untitled, White Ink on Black Paper 2013, 500cm x 100cm, 16.4ft x 3.2ft`,
+                    },
+                },
+                {
+                    p: {
+                        text: `Using the text piece as a starting point, laid to rest in darkness the light of her life, alongside my own curiosity into life and death, I began to think about how I could create a piece encompassing these interests.`,
+                    },
+                },
+                {
+                    p: {
+                        text: `The text piece, I found in a newspaper. The newspaper was dedicated to Princess Diana after she died. Alongside the projected text, there were two large scale drawings of my interpretation of light. One was black ink on white paper, the other being white ink on black paper.
                   Alongside the text pieces and drawings, there was a performance I created involving tissue paper and a overhead projector.`,
-					},
-				},
-				{
-					video: {
-                  poster: "../assets/project-2/4.jpg",
-                  src: "../assets/project-2/3.mov",
-                  text: ``,
-					},
-				},
-         ],
-      },
-		{
-			id: 3,
-			url: "pages/project_3.html",
-			thumb: "../assets/project-3/Thumb.jpg",
-         name: "Calasetta Calling",
-         mainImg: "../assets/project-3/1.jpg",
-			"image-title":
-				"Untitled, photograph from Calasetta Calling Performance ",
-			mainP: `Following on from the residency, I continued to collaborate with Stefano, Marina and Tankboys to do a one night performance at Stationers Hall. The performance piece was inspired by my wall drawing. I wanted to recreate the sonic, repetitive atmosphere, I was drawn to when I created the wall drawing. Calasetta Calling was a 3 hour durational performance.`,
-			componenets: [
-            {
-               p: {
-                  text: `Following on from the residency, I continued to collaborate with Stefano, Marina and Tankboys to do a one night performance at Stationers Hall. The performance piece was inspired by my wall drawing. I wanted to recreate the sonic, repetitive atmosphere, I was drawn to when I created the wall drawing. Calasetta Calling was a 3 hour durational performance.`,
-               },
-            },
-            {
-					video: {
-                  poster: "../assets/project-3/6.jpg",
-                  src: "../assets/project-3/5.mov",
-                  text: `Untitled, Calasetta Calling, audio`,
-                  p: true
-					},
-            },
-            {
-					p: {
-						imgUrl: "../assets/project-3/2.jpg",
-						text: `Untitled, Calasetta Calling, Wall Drawing detail, July 2013.`,
-					},
-				},
-				
-				{
-					p: {
-						text: `When I created this piece, I was very much drawn to the sound of the pencil marking the wall.`,
-					},
-				},
-				{
-					p: {
-                  imgUrl: "../assets/project-3/3.jpg",
-						text: `Untitled, Calasetta Calling, Wall Drawing detail, July 2013.`,
-					},
-				},
-				{
-					p: {
-						text: `Following on from the residency, I continued to collaborate with Stefano, Marina, Tankboys and ... to do a one night performance at Stationers Hall. The performance piece was inspired by my wall drawing. I wanted to recreate the sonic, repetitive atmosphere, I was drawn to when I created the wall drawing. Calasetta Calling was a 3 hour durational performance.`,
-					},
-				},
-            {
-					p: {
-                  imgUrl: "../assets/project-3/4.jpg",
-						text: `Untitled, Calasetta Calling, Wall Drawing detail, July 2013.`,
-					},
-				},
-         ],
-		},
-      { 
-         id: 4, 
-         url: "pages/project_4.html", 
-         thumb: "../assets/project-4/Thumb.jpg", 
-         name: "Brain",
-         mainImg: "../assets/project-4/1.jpg",
-			"image-title":
-				"Untitled, Photograph, 2013.",
-			mainP: ``,
-			componenets: [
-            {
-               p: {
-                  imgUrl: "../assets/project-4/2.jpg",
-                  text: `Untitled, 64 drawing installation, 2013.`,
-               },
-            },
-            {
-					audio: {
-                  src: "../assets/project-4/3.mp3",
-                  type:"audio/mpeg",
-                  text: `NW8 – SE8 London Exposure 13 Award Audio Extract 5 minutes 01 second duration of piece 3 hours 54 minutes 14 seconds.`,
-                  p: true
-					},
-            },
-         ],
-   },
-      { id: 5, 
-         url: "pages/project_5.html", 
-         thumb: "assets/project-5/Thumb.jpg", 
-         name: "SVA",
-         mainImg: "../assets/project-5/1.jpg",
-			"image-title":
-				"Untitled, Drawing on Maps, Newspaper and Magazine collage, 2014.",
-			mainP: ``,
-			componenets: [
-				{
-					p: {
-                  imgUrl: "../assets/project-5/2.jpg",
-						text: `Untitled, projection of hole punches on wall and wall drawing, 2014.`,
-               }
-            },{
-					p: {
-                  imgUrl: "../assets/project-5/3.jpg",
-						text: `Untitled, projection, 2015.`,
-               },
-            },{
-					p: {
-						text: `SVA Second Year`,
-               },
-               p: {
-                  imgUrl: "../assets/project-5/4.jpg",
-						text: `Untitled, Wall Drawings, 2015.`,
-               },
-            },{
+                    },
+                },
+                {
+                    video: {
+                        poster: "../assets/project-2/4.jpg",
+                        src: "../assets/project-2/3.mov",
+                        text: ``,
+                    },
+                },
+            ],
+        },
+        {
+            id: 3,
+            url: "pages/project_3.html",
+            thumb: "../assets/project-3/Thumb.jpg",
+            name: "Calasetta Calling",
+            mainImg: "../assets/project-3/1.jpg",
+            "image-title":
+                "Untitled, photograph from Calasetta Calling Performance ",
+            mainP: `Following on from the residency, I continued to collaborate with Stefano, Marina and Tankboys to do a one night performance at Stationers Hall. The performance piece was inspired by my wall drawing. I wanted to recreate the sonic, repetitive atmosphere, I was drawn to when I created the wall drawing. Calasetta Calling was a 3 hour durational performance.`,
+            componenets: [
+                {
+                    p: {
+                        text: `Following on from the residency, I continued to collaborate with Stefano, Marina and Tankboys to do a one night performance at Stationers Hall. The performance piece was inspired by my wall drawing. I wanted to recreate the sonic, repetitive atmosphere, I was drawn to when I created the wall drawing. Calasetta Calling was a 3 hour durational performance.`,
+                    },
+                },
+                {
+                    video: {
+                        poster: "../assets/project-3/6.jpg",
+                        src: "../assets/project-3/5.mov",
+                        text: `Untitled, Calasetta Calling, audio`,
+                        p: true
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-3/2.jpg",
+                        text: `Untitled, Calasetta Calling, Wall Drawing detail, July 2013.`,
+                    },
+                },
 
-               p: {
-                  imgUrl: "../assets/project-5/5.jpg",
-						text: `Untitled, Map Drawings, 2016.`,
-               },
-				},
-				
-         ],
-      },
-	],
+                {
+                    p: {
+                        text: `When I created this piece, I was very much drawn to the sound of the pencil marking the wall.`,
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-3/3.jpg",
+                        text: `Untitled, Calasetta Calling, Wall Drawing detail, July 2013.`,
+                    },
+                },
+                {
+                    p: {
+                        text: `Following on from the residency, I continued to collaborate with Stefano, Marina, Tankboys and ... to do a one night performance at Stationers Hall. The performance piece was inspired by my wall drawing. I wanted to recreate the sonic, repetitive atmosphere, I was drawn to when I created the wall drawing. Calasetta Calling was a 3 hour durational performance.`,
+                    },
+                },
+                {
+                    p: {
+                        imgUrl: "../assets/project-3/4.jpg",
+                        text: `Untitled, Calasetta Calling, Wall Drawing detail, July 2013.`,
+                    },
+                },
+            ],
+        },
+        {
+            id: 4,
+            url: "pages/project_4.html",
+            thumb: "../assets/project-4/Thumb.jpg",
+            name: "Brain",
+            mainImg: "../assets/project-4/1.jpg",
+            "image-title":
+                "Untitled, Photograph, 2013.",
+            mainP: ``,
+            componenets: [
+                {
+                    p: {
+                        imgUrl: "../assets/project-4/2.jpg",
+                        text: `Untitled, 64 drawing installation, 2013.`,
+                    },
+                },
+                {
+                    audio: {
+                        src: "../assets/project-4/3.mp3",
+                        type: "audio/mpeg",
+                        text: `NW8 – SE8 London Exposure 13 Award Audio Extract 5 minutes 01 second duration of piece 3 hours 54 minutes 14 seconds.`,
+                        p: true
+                    },
+                },
+            ],
+        },
+        // start of new project - start copying from hear
+        {
+            id: 5,
+            url: "pages/project_5.html",
+            thumb: "assets/project-5/Thumb.jpg",
+            name: "SVA",
+            mainImg: "../assets/project-5/1.jpg",
+            "image-title":
+                "Untitled, Drawing on Maps, Newspaper and Magazine collage, 2014.",
+            mainP: ``,
+            componenets: [
+                {
+                    p: {
+                        imgUrl: "../assets/project-5/2.jpg",
+                        text: `Untitled, projection of hole punches on wall and wall drawing, 2014.`,
+                    }
+                }, {
+                    p: {
+                        imgUrl: "../assets/project-5/3.jpg",
+                        text: `Untitled, projection, 2015.`,
+                    },
+                }, {
+                    p: {
+                        text: `SVA Second Year`,
+                    },
+                    p: {
+                        imgUrl: "../assets/project-5/4.jpg",
+                        text: `Untitled, Wall Drawings, 2015.`,
+                    },
+                }, {
+
+                    p: {
+                        imgUrl: "../assets/project-5/5.jpg",
+                        text: `Untitled, Map Drawings, 2016.`,
+                    },
+                },
+
+            ],
+        },
+        // until here
+        // start of new project - start copying from hear
+        {
+            id: 6,
+            url: "pages/project_6.html",
+            thumb: "assets/project-5/Thumb.jpg",
+            name: "SVA",
+            mainImg: "../assets/project-5/1.jpg",
+            "image-title":
+                "Untitled, Drawing on Maps, Newspaper and Magazine collage, 2014.",
+            mainP: ``,
+            componenets: [
+                {
+                    p: {
+                        imgUrl: "../assets/project-5/2.jpg",
+                        text: `Untitled, projection of hole punches on wall and wall drawing, 2014.`,
+                    }
+                }, {
+                    p: {
+                        imgUrl: "../assets/project-5/3.jpg",
+                        text: `Untitled, projection, 2015.`,
+                    },
+                }, {
+                    p: {
+                        text: `SVA Second Year`,
+                    },
+                    p: {
+                        imgUrl: "../assets/project-5/4.jpg",
+                        text: `Untitled, Wall Drawings, 2015.`,
+                    },
+                }, {
+
+                    p: {
+                        imgUrl: "../assets/project-5/5.jpg",
+                        text: `Untitled, Map Drawings, 2016.`,
+                    },
+                },
+
+            ],
+        },
+        // until here
+    ],
 };

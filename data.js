f0b30dd0b55603ef49845894dd444ff22d55daa8
ron erlih <ronerlih@gmail.com>
2020-11-16 10:41:47 -0500

    homepage content management

---
+++ data.js
@@ -0,0 +1,24 @@
+const homepage = {
+   projects: [
+      {  url: "project_1.html",
+         thumb: "assets/project-1/Thumb.jpg",
+         name: "Large Scale Drawing"
+      },
+      {  url: "project_2.html",
+         thumb: "assets/project-2/Thumb.jpg",
+         name: "Goldsmith Thesis"
+      },
+      {  url: "project_3.html",
+         thumb: "assets/project-3/Thumb.jpg",
+         name: "Calasetta Calling"
+      },
+      {  url: "project_4.html",
+         thumb: "assets/project-4/Thumb.jpg",
+         name: "Brain"
+      },
+      {  url: "project_5.html",
+         thumb: "assets/project-5/Thumb.jpg",
+         name: "SVA"
+      }
+   ]
+}
